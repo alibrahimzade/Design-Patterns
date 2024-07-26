ea9dd6e59c6211a69327dc69d22e14ad79d63b61
Ali <ibrahimzade.ali99@gmail.com>
2024-07-26 05:50:04 +0400

    Prototype pattern added

---
+++ src/az/ter/prototype/ShapeCache.java
@@ -0,0 +1,31 @@
+package az.ter.prototype;
+
+import java.util.Hashtable;
+
+public class ShapeCache {
+
+    private static Hashtable<String, Shape> shapeMap = new Hashtable<>();
+
+    public static Shape getShape(String shapeId){
+        Shape cachedShape = shapeMap.get(shapeId);
+        return (Shape) cachedShape.clone();
+    }
+
+    //TODO: for each shape run database query and create shape
+    //TODO: shapeMApe.put(ShapeKey, shape);
+    //TODO: for example, we are adding three shapes
+
+    public static void loadCache(){
+        Circle circle = new Circle();
+        circle.setId("1");
+        shapeMap.put(circle.getId(), circle);
+
+        Square square = new Square();
+        square.setId("2");
+        shapeMap.put(square.getId(),square);
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.setId("3");
+        shapeMap.put(rectangle.getId(), rectangle);
+    }
+}
